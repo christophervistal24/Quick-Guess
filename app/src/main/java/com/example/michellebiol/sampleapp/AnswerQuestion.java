4dd4ee7ed32e9257712a84d4fdb28f12a68c12b5
= <=christophervistal24@gmail.com>
2018-07-23 22:25:54 +0800

    With Share Link

--- app/src/main/java/com/example/michellebiol/sampleapp/AnswerQuestion.java
+++ app/src/main/java/com/example/michellebiol/sampleapp/AnswerQuestion.java
@@ -3,11 +3,19 @@ package com.example.michellebiol.sampleapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -18,7 +26,18 @@ import com.example.michellebiol.sampleapp.Interfaces.IAnswerApi;
 import com.example.michellebiol.sampleapp.Models.AnswerRequest;
 import com.example.michellebiol.sampleapp.Models.AnswerResponse;
 import com.example.michellebiol.sampleapp.Models.QuestionsItem;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
+import org.w3c.dom.Text;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 import retrofit2.Call;
@@ -29,23 +48,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AnswerQuestion extends AppCompatActivity {
 
-    TextView givenQuestionId;
-    TextView givenQuestion;
-    TextView correctAnswer;
-    RadioButton choiceA;
-    RadioButton choiceB;
-    RadioButton choiceC;
-    RadioButton choiceD;
+    TextView givenQuestionId,
+            givenQuestion ,
+            correctAnswer ,
+            timerCountDown ,
+            questionFunFacts ,
+            answerResult ,
+            currentQuestionAndTotal;
+    RadioButton choiceA , choiceB , choiceC , choiceD , radioButton;
     RadioGroup RGroup;
-    RadioButton radioButton;
-    TextView timerCountDown;
-    static final int counter = 20;
+    private static final int counter = 20;
     CountDownTimer countDownTimer;
-    Button nextStage;
+    Button confirmQuestion ,
+            nextStage ,
+            btnShareOnFacebook;
     IAnswerApi services;
     Intent i;
     static int intPosition;
     QuestionsItem question;
+    LinearLayout questionLayout;
+    LinearLayout questionResult;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
 
 
 
@@ -57,23 +81,108 @@ public class AnswerQuestion extends AppCompatActivity {
         givenQuestionId = (TextView) findViewById(R.id.givenQuestionId);
         givenQuestion = (TextView) findViewById(R.id.givenQuestion);
         correctAnswer = (TextView) findViewById(R.id.correctAnswer);
+        questionFunFacts = (TextView) findViewById(R.id.questionFunFacts);
+        answerResult = (TextView) findViewById(R.id.answerResult);
+        currentQuestionAndTotal = (TextView) findViewById(R.id.currentQuestionAndTotal);
+
         choiceA = (RadioButton) findViewById(R.id.choice_a);
         choiceB = (RadioButton) findViewById(R.id.choice_b);
         choiceC = (RadioButton) findViewById(R.id.choice_c);
         choiceD = (RadioButton) findViewById(R.id.choice_d);
-        RGroup = (RadioGroup) findViewById(R.id.RGroup);
+        RGroup  = (RadioGroup) findViewById(R.id.RGroup);
+
+        questionLayout = (LinearLayout) findViewById(R.id.questionLayout);
+        questionResult = (LinearLayout) findViewById(R.id.questionResult);
+
+        //Init FB
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
         final QuestionsItem question;
         i =  getIntent();
 
         timerCountDown = (TextView) findViewById(R.id.timerCountDown);
-        nextStage = (Button) findViewById(R.id.nextStage);
+
+        confirmQuestion     = (Button) findViewById(R.id.confirmQuestion);
+        nextStage            = (Button) findViewById(R.id.nextStage);
+        btnShareOnFacebook  = (Button) findViewById(R.id.btnShareOnFacebook);
+
+
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(getString(R.string.user_api_url))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         services = retrofit.create(IAnswerApi.class);
+
         setTimer(counter);
-        setQuestionAndAnswer();
+
+        setAndGetQuestionAnswer();
+
+        nextStage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                proceedToNextQuestion();
+                resumeAnswering();
+            }
+        });
+
+        btnShareOnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+                   @Override
+                   public void onSuccess(Sharer.Result result) {
+                       Toast.makeText(AnswerQuestion.this, "Successfully share!", Toast.LENGTH_SHORT)
+                               .show();
+                   }
+
+                   @Override
+                   public void onCancel() {
+                       Toast.makeText(AnswerQuestion.this, "Share cancel", Toast.LENGTH_SHORT)
+                               .show();
+                   }
+
+                   @Override
+                   public void onError(FacebookException error) {
+                       Toast.makeText(AnswerQuestion.this, error.getMessage(), Toast.LENGTH_SHORT)
+                               .show();
+                   }
+               });
+
+                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setQuote("We are still working on it.")
+                        .setContentUrl(Uri.parse("https://quick-guess.herokuapp.com/"))
+                        .setShareHashtag(new ShareHashtag.Builder()
+                                .setHashtag("#Quick-Guess Fun Facts")
+                                .build())
+                        .build();
+
+                if (ShareDialog.canShow(ShareLinkContent.class))
+                {
+                    shareDialog.show(linkContent);
+                }
+            }
+        });
+
+    }
+
+  private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.michellebiol.sampleapp",
+                    PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures)
+            {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("Keyhash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setTimer(Integer counter) {
@@ -90,21 +199,50 @@ public class AnswerQuestion extends AppCompatActivity {
         }.start();
     }
 
-    private void setQuestionAndAnswer() {
-        String position = i.getStringExtra("current_position");
-        intPosition = Integer.parseInt(position);
-        question = QuestionsAdapter.questionsItems.get(intPosition);
+    //get the question info.
+    private void getQuestionInformation()
+    {
+        currentQuestionAndTotal.setText("Question : " + String.valueOf(intPosition+1) + " / " + QuestionsAdapter.questionsItems.size());
         givenQuestionId.setText("Question ID : " + question.getId());
+
         givenQuestion.setText("Question : " + question.getQuest());
-        String[] arr = {question.getChoice_a(),question.getChoice_b(),question.getChoice_c(),question.getChoice_d()};
+
+        String[] arr = {
+                question.getChoice_a(),
+                question.getChoice_b(),
+                question.getChoice_c(),
+                question.getChoice_d()
+        };
+
         int n = arr.length;
+
         String[] shuffledChoices = randomize(arr,n);
+
         choiceA.setText(shuffledChoices[0]);
         choiceB.setText(shuffledChoices[1]);
         choiceC.setText(shuffledChoices[2]);
         choiceD.setText(shuffledChoices[3]);
     }
 
+    private void setAndGetQuestionAnswer() {
+        String position = i.getStringExtra("current_position");
+
+        intPosition = Integer.parseInt(position);
+
+        question = QuestionsAdapter.questionsItems.get(intPosition);
+
+        getQuestionInformation();
+
+        confirmQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSelected();
+            }
+        });
+
+    }
+
+    //randomize the choices
     private String[] randomize(String arr[] , int n)
     {
         Random r = new Random();
@@ -122,7 +260,20 @@ public class AnswerQuestion extends AppCompatActivity {
     }
 
 
-    public void getSelected(View v)
+    private void proceedToNextQuestion()
+    {
+        intPosition++;
+        countDownTimer.cancel();
+        isFinish(intPosition);
+        getQuestionInformation();
+        RGroup.clearCheck();
+        setTimer(counter);
+
+
+    }
+
+    //get the selected answer of the user
+    public void getSelected()
     {
         int radioId = RGroup.getCheckedRadioButtonId();
         radioButton = findViewById(radioId);
@@ -132,31 +283,46 @@ public class AnswerQuestion extends AppCompatActivity {
         }
         else
         {
-            Toast.makeText(this, "Result : " + isCorrect(radioButton.getText().toString()) , Toast.LENGTH_SHORT).show();
-            proceedToNextQuestion();
+
+            answerResult.setText( "Results : " + isCorrect(radioButton.getText().toString()));
             displayFunFacts();
         }
     }
 
-    private void proceedToNextQuestion()
+    private void displayResult()
     {
-        intPosition++;
-        isFinish(QuestionsAdapter.questionsItems.size());
+        nextStage.setVisibility(View.VISIBLE);
+        btnShareOnFacebook.setVisibility(View.VISIBLE);
         countDownTimer.cancel();
-        question = QuestionsAdapter.questionsItems.get(intPosition);
-        givenQuestionId.setText("Question ID : " + question.getId());
-        givenQuestion.setText("Question : " + question.getQuest());
-        String[] arr = {question.getChoice_a(),question.getChoice_b(),question.getChoice_c(),question.getChoice_d()};
-        int n = arr.length;
-        String[] shuffledChoices = randomize(arr,n);
-        choiceA.setText(shuffledChoices[0]);
-        choiceB.setText(shuffledChoices[1]);
-        choiceC.setText(shuffledChoices[2]);
-        choiceD.setText(shuffledChoices[3]);
-        setTimer(counter);
+        questionLayout.setVisibility(View.INVISIBLE);
+        questionResult.setVisibility(View.VISIBLE);
 
-        //method that check if the user reach the max questions
 
+    }
+
+
+    private void resumeAnswering()
+    {
+        nextStage.setVisibility(View.INVISIBLE);
+        btnShareOnFacebook.setVisibility(View.INVISIBLE);
+        questionLayout.setVisibility(View.VISIBLE);
+        questionResult.setVisibility(View.INVISIBLE);
+        countDownTimer.start();
+
+    }
+
+    // if the question category is already finish
+    private void isFinish(int intPosition)
+    {
+
+        if (intPosition+1 <= QuestionsAdapter.questionsItems.size())
+        {
+            question = QuestionsAdapter.questionsItems.get(intPosition);
+        } else
+        {
+            currentQuestionAndTotal.setVisibility(View.INVISIBLE);
+            this.finish();
+        }
     }
 
 
@@ -168,12 +334,14 @@ public class AnswerQuestion extends AppCompatActivity {
         {
             result =  "Correct";
             sendAnswer(result);
+            displayResult();
             return result;
         }
         else
         {
             result = "Wrong";
             sendAnswer(result);
+            displayResult();
             return result;
         }
     }
@@ -199,31 +367,28 @@ public class AnswerQuestion extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    private String[] initIntent()
+
+    //display the fun facts of the question that the user choose
+    private void displayFunFacts()
     {
+        questionFunFacts.setText("Fun facts :" + question.getFun_facts());
+        displayResult();
 
+    }
 
+    //send the user status to the database using retrofit
+    private void sendAnswer(String result)
+    {
+        //get the user token &
         SharedPreferences sharedPref = getSharedPreferences("tokens", Context.MODE_PRIVATE);
         String token_type = sharedPref.getString("token_type","");
         String token = sharedPref.getString("token","");
 
-        return new String[]{token_type,token,question.getCorrect_answer(),question.getId()};
-    }
-
-
-    private void displayFunFacts()
-    {
-//        Toast.makeText(this, "Fun Facts : " + question.getFun_facts(), Toast.LENGTH_SHORT).show();
-    }
-
-    private void sendAnswer(String result)
-    {
-        String[] intentValue = initIntent();
         final AnswerRequest answerRequest = new AnswerRequest();
         answerRequest.setQuestion_id(Integer.parseInt(question.getId()));
         answerRequest.setQuestion_result(result);
 
-        Call<AnswerResponse> answerResponseCall = services.userAnswer(intentValue[0].concat(intentValue[1]),answerRequest);
+        Call<AnswerResponse> answerResponseCall = services.userAnswer(token_type.concat(token),answerRequest);
         answerResponseCall.enqueue(new Callback<AnswerResponse>() {
             @Override
             public void onResponse(Call<AnswerResponse> call, Response<AnswerResponse> response) {
--- app/src/main/java/com/example/michellebiol/sampleapp/HomeActivity.java
+++ app/src/main/java/com/example/michellebiol/sampleapp/HomeActivity.java
@@ -41,12 +41,8 @@ public class HomeActivity extends AppCompatActivity implements PlayerNameDialog.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this,mdrawerLayout,R.string.open,R.string.close);
+        hideNavigationBar();
         btnCategory = (Button) findViewById(R.id.btnCategory);
-        mdrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
         isTokenAlreadySet();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(getString(R.string.user_api_url))
@@ -54,19 +50,25 @@ public class HomeActivity extends AppCompatActivity implements PlayerNameDialog.
                 .build();
         services = retrofit.create(IRegisterUserApi.class);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume()
+    {
+        super.onResume();
+        hideNavigationBar();
     }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+        .setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
+
 
     public void categories(View view)
     {
